de0d360f6e083d679e69c79f34e9b18c0c91e330
giantrotta24 <giantrotta@me.com>
2019-01-29 16:01:01 -0500

    finished manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -25,6 +25,7 @@ connection.connect(function (err) {
     startApp();
 });
 
+//start app
 function startApp() {
     console.log('\n');
     console.log('\n');
@@ -64,7 +65,9 @@ function startApp() {
     promptOptions();
 }
 
+//app menu with switch function
 function promptOptions() {
+    //prompt user
     inquirer.prompt({
         name: "menu",
         type: "list",
@@ -86,8 +89,7 @@ function promptOptions() {
                 break;
 
             case "Add New Product".magenta:
-                console.log("nothing yet");
-                connection.end();
+                addItem();
                 break;
 
             case "Exit".red:
@@ -103,7 +105,61 @@ function validateInt(num) {
     return reg.test(num) || "Please enter a valid ID.".red;
 }
 
+//add new item
 function addItem() {
+    // prompt user to grab item name, department, price, quantity
+    inquirer.prompt([
+        {
+            name: "item",
+            type: "input",
+            message: "What is the item you would like to submit?"
+        },
+        {
+            name: "department",
+            type: "input",
+            message: "What department would you like to add your item to?"
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "What is the stock price?",
+            //validate that price and quantity are valid numbers
+            validate: function (num) {
+                if (isNaN(num) === false) {
+                    return true;
+                }
+                return "Not a valid price.";
+            }
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Enter a quantity.",
+            validate: function (num) {
+                if (isNaN(num) === false) {
+                    return true;
+                }
+                return "Not a valid  quantity.";
+            }
+        }
+    ]).then(function (answer) {
+        // insert into database and loop app to start
+        let insert = "INSERT INTO products SET ?";
+        connection.query(insert, { product_name: answer.item, department_name: answer.department, price: answer.price, stock_quantity: answer.quantity }, function (err) {
+            if (err) throw err;
+            console.log('\n');
+            console.log('--------------------'.blue);
+            console.log("Product restock successful".blue);
+            console.log('--------------------'.blue);
+            console.log('\n');
+            promptOptions();
+        });
+    });
+}
+
+//add stock quantity
+function addStock() {
+    // prompt for item
     inquirer.prompt([
         {
             name: "idInput",
@@ -112,6 +168,7 @@ function addItem() {
             message: "Enter the ID # of the item you want to restock.".blue
         }
     ]).then(function (answer) {
+        // item id cant be 0
         if (answer.idInput === "0") {
             console.log('--------------------'.blue);
             console.log('\n');
@@ -120,6 +177,7 @@ function addItem() {
             console.log('--------------------'.blue);
             addItem();
         } else {
+            // prompt for quantity 
             inquirer.prompt([
                 {
                     name: "quantity",
@@ -128,6 +186,7 @@ function addItem() {
                     message: "How many of this item would you like to add?".blue
                 }
             ]).then(function (ans) {
+                // cant be 0
                 if (ans.quantity === "0") {
                     console.log('--------------------'.blue);
                     console.log('\n');
@@ -136,25 +195,32 @@ function addItem() {
                     console.log('--------------------'.blue);
                     addItem();
                 } else {
+                    // grab answer ID and pull from database
                     let select = "SELECT products.product_name, products.price, products.stock_quantity FROM products WHERE ?";
                     connection.query(select, [{ item_id: answer.idInput }], function (err, res) {
                         if (err) throw err;
+                        // grab quantity
                         let newQuantity = res[0].stock_quantity;
+                        // update query
                         let update = "UPDATE products SET ? WHERE ?";
                         console.log('--------------------'.blue);
                         console.log('\n');
                         console.log("Stock Quantity:".red)
                         console.log(newQuantity);
+                        // add user quantity to stock quantity
                         newQuantity += parseInt(ans.quantity);
                         console.log('--------------------'.blue);
                         console.log("Stock Quantity after Update:".green)
                         console.log(newQuantity);
+                        // update database
                         connection.query(update, [{ stock_quantity: newQuantity }, { item_id: answer.idInput }], function (err, res) {
                             if (err) throw err;
+                            // alert user
                             console.log('--------------------'.blue);
                             console.log("Product restock successful".blue);
                             console.log('--------------------'.blue);
                             console.log('\n');
+                            // loop
                             promptOptions();
                         });
                     });
@@ -164,7 +230,9 @@ function addItem() {
     });
 }
 
+//display low quantity items
 function queryLow() {
+    // find items in database
     let select = "SELECT products.item_id, products.product_name, products.price, products.stock_quantity FROM products WHERE stock_quantity < 5";
     connection.query(select, function (err, res) {
 
@@ -181,12 +249,14 @@ function queryLow() {
             console.log('\n');
 
         }
-        //trigger purchase prompt
+        //trigger prompt
         promptOptions();
     });
 }
 
+//display all item function for add item function
 function displayAll() {
+    // pull from database
     let select = "SELECT * FROM products";
     connection.query(select, function (err, res) {
 
@@ -203,11 +273,14 @@ function displayAll() {
             console.log('\n');
 
         }
-        addItem();
+        //add function
+        addStock();
     });
 }
 
+//display all items from user query
 function queryAll() {
+    // pull from database
     let select = "SELECT * FROM products";
     connection.query(select, function (err, res) {
 
@@ -224,7 +297,7 @@ function queryAll() {
             console.log('\n');
 
         }
-        //trigger purchase prompt
+        //trigger prompt
         promptOptions();
     });
 
